5d2a5908a604146ad18ba6a2dab04dd44c48f073
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-05-01 22:17:47 +0530

    add views

---
+++ views/templates.go
@@ -0,0 +1,30 @@
+package views
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("Error while parsing:%v", err)
+	}
+	return Template{
+		htmlTpl: tpl,
+	}, nil
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.htmlTpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Error while executing template:%v", err)
+		http.Error(w, "Error while executing template", http.StatusInternalServerError)
+	}
+}
