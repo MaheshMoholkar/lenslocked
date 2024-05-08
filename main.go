d502a387af8361a7997457bee914e69f5249ff50
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-05-08 21:34:32 +0530

    add dynamic faq page

--- main.go
+++ main.go
@@ -19,6 +19,9 @@ func main() {
 	tpl = views.Must(views.ParseFS(templates.FS, "about.html"))
 	r.Get("/about", controllers.StaticHandler(tpl))
 
+	tpl = views.Must(views.ParseFS(templates.FS, "faq.html"))
+	r.Get("/faq", controllers.FAQ(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
